a2c0bad8ab077dd6971aaa9282c1cd8075c7556e
Jonas Davila da Silva <161352305+jonasqasoftware@users.noreply.github.com>
2026-08-26 18:54:02 -0300

    build: habilita exportação estática

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    tsconfigPath: "tsconfig.pages.json",
+  },
 };
 
 export default nextConfig;
